9b214acaf631bbe8eb94129b8cd99a67e2fb075d
eamoses <e.a.moses@gmail.com>
2022-10-26 19:49:43 -0500

    save

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,11 +1,18 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  if (num % 2 === 0) {
+    return true
+  } else {
+    return false
+  }
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  let sum = arr.reduce(function(a,b){
+    return a+b
+  })
+  return sum
 }
 
 const comboSum = (arr, sum) => {
